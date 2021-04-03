64f74927136a5111805cddd89e8dcdebf00d4c97
quantum-mob <quantum.mob21@gmail.com>
2021-04-04 01:55:10 +0900

    Public release

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,11 @@
 (* Paclet Info File *)
 
-(* $Date: 2021-04-03 19:16:43+09 $ *)
-(* $Revision: 1.1 $ *)
+(* $Date: 2021-04-04 01:54:53+09 $ *)
+(* $Revision: 1.2 $ *)
 
 Paclet[
   Name -> "QuantumWorkbook",
-  Version -> "1.0.0",
+  Version -> "1.0.1",
   WolframVersion -> "12+",
   Description -> "Mathematica Notebook files accompanying the Quantum Computation Workbook.",
   Creator -> "Mahn-Soo Choi (Korea University)",
